f0e4b839ca95440d35cdbfa84a583ffdc4476f09
qin yu peng <qin.batista@outlook.com>
2020-03-17 18:16:18 +0800

    可以打出完整包

--- MercuryAPKProject/BuildAPK.py
+++ MercuryAPKProject/BuildAPK.py
@@ -9,11 +9,15 @@ def main():
     #PythonFunction.FuncFunctionList.CleanCache()
     #PythonFunction.FuncFunctionList.RestSetting()
 	os.chdir(PythonLocation())
-	if os.path.isfile(PythonLocation()+"/app-release.apk"):
-		os.remove("app-release.apk")
+	if os.path.isfile("./app-release.apk"):
+		os.remove("./app-release.apk")
 	os.system("gradle clean assemblerelease")
-	if os.path.isfile(PythonLocation()+"/app/build/outputs/apk/release/app-release.apk"):
-		os.rename(PythonLocation()+"/app/build/outputs/apk/release/app-release.apk",PythonLocation()+"/app-release.apk")
+	print("------------")
+	# print(PythonLocation())
+	print("./app/build/outputs/apk/release/app-release.apk")
+	os.system("pwd")
+	if os.path.isfile("./app/build/outputs/apk/release/app-release.apk"):
+		os.rename("./app/build/outputs/apk/release/app-release.apk","./app-release.apk")
 
 if __name__ == '__main__':
     main()
