b536deeddb9d7924705b54a29372b05933566148
qin yu peng <qin.batista@outlook.com>
2020-03-27 17:10:25 +0800

    更新jar生成逻辑

--- Android_Umeng/MercuryJarProject/BuildJAR.py
+++ Android_Umeng/MercuryJarProject/BuildJAR.py
@@ -4,26 +4,38 @@ import platform
 import shutil
 def PythonLocation():
 	return os.path.dirname(os.path.realpath(__file__))
+
+def delete_folder(src):
+	'''delete files and folders'''
+	if os.path.isfile(src):
+		try:
+			os.remove(src)
+		except:
+			pass
+	elif os.path.isdir(src):
+		for item in os.listdir(src):
+			itemsrc=os.path.join(src,item)
+			delete_folder(itemsrc) 
+		try:
+			os.rmdir(src)
+		except:
+			pass
+
 def main():
 	#PythonFunction.FuncFunctionList.CleanCache()
 	#PythonFunction.FuncFunctionList.RestSetting()
 	os.chdir(PythonLocation())
-	if os.path.isfile("./UnityPlugin.jar"):
-		os.remove("./UnityPlugin.jar")
-	if os.path.isfile("./classes.jar"):
-		os.remove("./classes.jar")
-	if os.path.isfile("./R.txt"):
-		os.remove("./R.txt")
-	if os.path.isfile("./AndroidManifest.xml"):
-		os.remove("./AndroidManifest.xml")
 	os.system("gradle clean makejar")
 	os.system('pwd')
-	if os.path.isfile("./mercury/build/outputs/aar/mercury-release.aar"):
-		os.system("unzip ./mercury/build/outputs/aar/mercury-release.aar")
-		os.rename("./classes.jar","./MercurySDK.jar")
-		if os.path.isfile("./../../Plugin_UnityJar/UnityJarProject/libs/MercurySDK.jar"):
-			os.remove("./../../Plugin_UnityJar/UnityJarProject/libs/MercurySDK.jar")
-		shutil.copy("./MercurySDK.jar", "./../../Plugin_UnityJar/UnityJarProject/libs/MercurySDK.jar")
+	if os.path.exists("./cache"):delete_folder("./cache")
+	os.system("mkdir cache")
+	os.chdir(PythonLocation()+"/cache")
+	if os.path.isfile("./../mercury/build/outputs/aar/mercury-release.aar"):
+		os.system("unzip ./../mercury/build/outputs/aar/mercury-release.aar")
+		os.rename("./classes.jar","./../MercurySDK.jar")
+		if os.path.isfile("./../../../Plugin_UnityJar/UnityJarProject/libs/MercurySDK.jar"):
+			os.remove("./../../../Plugin_UnityJar/UnityJarProject/libs/MercurySDK.jar")
+		shutil.copy("./../MercurySDK.jar", "./../../../Plugin_UnityJar/UnityJarProject/libs/MercurySDK.jar")
 
 
 if __name__ == '__main__':
