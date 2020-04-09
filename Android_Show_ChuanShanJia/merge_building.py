c214b669b254b17ec04e6efeb955b1837963a3b3
qin yu peng <qin.batista@outlook.com>
2020-04-09 15:46:02 +0800

    同步sdk

--- Android_Show_ChuanShanJia/merge_building.py
+++ Android_Show_ChuanShanJia/merge_building.py
@@ -361,11 +361,11 @@ class APKBuildManager():
 
 
 def run():
-	if os.path.isdir(f"{PythonLocation()}/MercuryAPKProject_pure")
-		os.sysmte(f"rm -rf {PythonLocation()}/MercuryAPKProject")
-		os.system(f"cp {PythonLocation()}/MercuryAPKProject_pure {PythonLocation()}/MercuryAPKProject")
+	if os.path.isdir(f"{PythonLocation()}/MercuryAPKProject_pure"):
+		if os.path.isdir(f"{PythonLocation()}/MercuryAPKProject"):shutil.rmtree(f"{PythonLocation()}/MercuryAPKProject")
+		shutil.copytree(f"{PythonLocation()}/MercuryAPKProject_pure",f"{PythonLocation()}/MercuryAPKProject")
 	else:
-		os.system(f"cp {PythonLocation()}/MercuryAPKProject {PythonLocation()}/MercuryAPKProject_pure")
+		shutil.copytree(f"{PythonLocation()}/MercuryAPKProject",f"{PythonLocation()}/MercuryAPKProject_pure")
 	sam = APKBuildManager()
 	sam.merge_sdk_resource()
 
