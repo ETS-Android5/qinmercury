ac3beab2d6179c9f5841d671c64989ef8ef6e498
qin yu peng <qin.batista@outlook.com>
2020-04-23 15:17:31 +0800

    更新sdk

--- android_builder.py
+++ android_builder.py
@@ -283,9 +283,9 @@ class SDKAppendManager():
 		return self.__file_path+"/Y_building/"+file_name+"_"+self.__channel+file_format
 def run():
 	file_path =  os.path.splitext(__file__)[0][os.path.splitext(__file__)[0].rfind("/")+1:]
-	if os.path.isfile(PythonLocation()+"/"+file_path+".py"):
-		os.remove(PythonLocation()+"/"+file_path+".py")
 	if os.path.isfile(PythonLocation()+"/z_PythonCode/"+file_path+".py"):
+		if os.path.isfile(PythonLocation()+"/"+file_path+".py"):
+			os.remove(PythonLocation()+"/"+file_path+".py")
 		shutil.copy(PythonLocation()+"/z_PythonCode/"+file_path+".py",PythonLocation()+"/"+file_path+".py")
 
 	folder_name = os.path.splitext(__file__)[0][os.path.splitext(__file__)[0].rfind("/")+1:]
