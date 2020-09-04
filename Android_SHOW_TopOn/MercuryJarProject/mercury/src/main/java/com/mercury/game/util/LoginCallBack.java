5f78da310e6a0562b4fca4a28c8a615de659f3d5
qin yu peng <qin.batista@outlook.com>
2020-09-04 14:29:03 +0800

    更新

---
+++ Android_SHOW_TopOn/MercuryJarProject/mercury/src/main/java/com/mercury/game/util/LoginCallBack.java
@@ -0,0 +1,11 @@
+package com.mercury.game.util;
+
+/**
+ * @Author: Risemy
+ * Create Time:2020/7/30
+ */
+public interface LoginCallBack {
+    void success(String msg);
+
+    void fail(String msg);
+}
