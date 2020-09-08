aef1953f98521ae1760daeaacca044057d83fecc
qin yu peng <qin.batista@outlook.com>
2020-09-08 13:45:24 +0800

    更新sdk

--- Android_SHOW_TopOn_V2/MercuryJarProject/mercury/src/main/java/com/mercury/game/util/SpConfig.java
+++
@@ -1,8 +0,0 @@
-package com.mercury.game.util;
-
-public class SpConfig {
-    public static final String USER_ID = "USER_ID";
-    public static final String USER_PHONE = "USER_PHONE";
-    public static final String USER_CARD_ID = "USER_CARD_ID";
-    public static final String TOKEN = "TOKEN";
-}
