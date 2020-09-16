a351042dcdfef3b3273c9fbc909579efea9b88a9
qin yu peng <qin.batista@outlook.com>
2020-09-16 17:15:19 +0800

    更新

--- Android_IAP_AlipayWepayWIthTapTapLogin/MercuryJarProject/mercury/src/main/java/com/mercury/game/InAppChannel/InAppChannel.java
+++ Android_IAP_AlipayWepayWIthTapTapLogin/MercuryJarProject/mercury/src/main/java/com/mercury/game/InAppChannel/InAppChannel.java
@@ -32,9 +32,11 @@ import com.mercury.game.MercuryActivity;
 import com.mercury.game.util.LoginCallBack;
 import com.mercury.game.util.MD5;
 import com.mercury.game.util.MercuryConst;
+import com.taptap.sdk.AccessToken;
 import com.taptap.sdk.CallBackManager;
 import com.taptap.sdk.LoginManager;
 import com.taptap.sdk.LoginResponse;
+import com.taptap.sdk.Profile;
 import com.taptap.sdk.TapTapLoginCallback;
 import com.taptap.sdk.TapTapSdk;
 import com.tencent.mm.opensdk.modelpay.PayReq;
@@ -292,15 +294,11 @@ public class InAppChannel extends InAppBase {
 		return false;
 	}
 	private  class GetPrepayIdTask extends AsyncTask<Void, Void, Map<String,String>> {
-
 		private ProgressDialog dialog;
-
-
 		@Override
 		protected void onPreExecute() {
 			dialog = ProgressDialog.show(mContext,"正在生成订单", "请稍后！");
 		}
-
 		@Override
 		protected void onPostExecute(Map<String,String> result) {
 			if (dialog != null) {
@@ -310,42 +308,33 @@ public class InAppChannel extends InAppBase {
 			MercuryActivity.LogLocal("[InAppChannel][onPostExecute]result = "+result);
 			resultunifiedorder=result;
 			WXSendReq();
-
 		}
-
 		@Override
 		protected void onCancelled() {
 			super.onCancelled();
 		}
-
 		@Override
 		protected Map<String,String>  doInBackground(Void... params) {
-
 			String url = String.format("https://api.mch.weixin.qq.com/pay/unifiedorder");
 			String entity = genProductArgs();
 			MercuryActivity.LogLocal("[InAppChannel][GetPrepayIdTask]entity = "+entity);
-
 			byte[] buf = httpPost(url, entity);
 			String content = new String(buf);
 			MercuryActivity.LogLocal("[InAppChannel][GetPrepayIdTask]entity = "+entity);
 			Map<String,String> xml=decodeXml(content);
-
 			return xml;
 		}
 	}
 	public  Map<String,String> decodeXml(String content) {
-
 		try {
 			Map<String, String> xml = new HashMap<String, String>();
 			XmlPullParser parser = Xml.newPullParser();
 			parser.setInput(new StringReader(content));
 			int event = parser.getEventType();
 			while (event != XmlPullParser.END_DOCUMENT) {
-
 				String nodeName=parser.getName();
 				switch (event) {
 					case XmlPullParser.START_DOCUMENT:
-
 						break;
 					case XmlPullParser.START_TAG:
 
@@ -359,7 +348,6 @@ public class InAppChannel extends InAppBase {
 				}
 				event = parser.next();
 			}
-
 			return xml;
 		} catch (Exception e) {
 			MercuryActivity.LogLocal("[InAppChannel][genProductArgs] decodeXml = " + e.toString());
@@ -382,16 +370,13 @@ public class InAppChannel extends InAppBase {
 			packageParams.add(new BasicNameValuePair("mch_id", WX_MCH_ID));
 			packageParams.add(new BasicNameValuePair("nonce_str", nonceStr));
 			packageParams.add(new BasicNameValuePair("notify_url","weixinCPS"));
-
 			packageParams.add(new BasicNameValuePair("out_trade_no",order_id));
 			packageParams.add(new BasicNameValuePair("spbill_create_ip","127.0.0.1"));
-
 			packageParams.add(new BasicNameValuePair("total_fee", strPriceString));
 			packageParams.add(new BasicNameValuePair("trade_type", "APP"));
 			String sign = genPackageSign(packageParams);
 			packageParams.add(new BasicNameValuePair("sign", sign));
 			String xmlstring =toXml(packageParams);
-
 			return new String(xmlstring.toString().getBytes(), "ISO8859-1");
 
 		} catch (Exception e) {
@@ -410,8 +395,6 @@ public class InAppChannel extends InAppBase {
 		}
 		sb.append("key=");
 		sb.append(WX_API_KEY);
-
-
 		String packageSign = MD5.getMessageDigest(sb.toString().getBytes()).toUpperCase();
 		return packageSign;
 	}
@@ -421,8 +404,6 @@ public class InAppChannel extends InAppBase {
 		sb.append("<xml>");
 		for (int i = 0; i < params.size(); i++) {
 			sb.append("<"+params.get(i).getName()+">");
-
-
 			sb.append(params.get(i).getValue());
 			sb.append("</"+params.get(i).getName()+">");
 		}
@@ -443,15 +424,16 @@ public class InAppChannel extends InAppBase {
 			@Override
 			public void onSuccess(LoginResponse loginResult) {
 				// TODO:登录成功
-				LoginSuccessCallBack("");
-			}
+				LogLocal("[InAppChannel][SingmaanLogin]Profile.getCurrentProfile()=" + Profile.getCurrentProfile());
+				LogLocal("[InAppChannel][SingmaanLogin]AccessToken.getCurrentAccessToken()=" + AccessToken.getCurrentAccessToken());
 
+				LoginSuccessCallBack(loginResult.toString());
+			}
 			@Override
 			public void onCancel() {
 				// TODO:用户取消
 				LoginCancelCallBack("");
 			}
-
 			@Override
 			public void onError(Throwable throwable) {
 				// TODO:登录失败
@@ -459,8 +441,11 @@ public class InAppChannel extends InAppBase {
 			}
 		});
 		LoginManager.getInstance().logInWithReadPermissions(mContext, TapTapSdk.SCOPE_PUIBLIC_PROFILE);
+
+
 		//shrinkpartend
 	}
+
 	public  void WXSendReq()
 	{
 		genPayReq();
@@ -468,7 +453,6 @@ public class InAppChannel extends InAppBase {
 		msgApi.sendReq(req);
 	}
 	private  void genPayReq() {
-
 		req.appId = WX_APP_ID;
 		req.partnerId = WX_MCH_ID;
 		req.prepayId = resultunifiedorder.get("prepay_id");
@@ -485,11 +469,9 @@ public class InAppChannel extends InAppBase {
 		req.sign = genAppSign(signParams);
 		sb.append("sign\n"+req.sign+"\n\n");
 		LogLocal("[InAppChannel][genPayReq] signParams.toString()="+signParams.toString());
-
 	}
 	private  String genAppSign(List<NameValuePair> params) {
 		StringBuilder sb = new StringBuilder();
-
 		for (int i = 0; i < params.size(); i++) {
 			sb.append(params.get(i).getName());
 			sb.append('=');
@@ -510,6 +492,7 @@ public class InAppChannel extends InAppBase {
 	public void SingmaanLogout()
 	{
 		LogLocal("[MercuryActivity][SingmaanLogout]"+DeviceId);
+		LoginManager.getInstance().logout();
 		LoginCancelCallBack(DeviceId);
 	}
 
@@ -563,7 +546,6 @@ public class InAppChannel extends InAppBase {
 	{
 		MercuryActivity.LogLocal("["+Channelname+"][onPause]");
 	}
-	
 	@Override
 	public void onResume()
 	{
@@ -597,6 +579,8 @@ public class InAppChannel extends InAppBase {
 	public void onActivityResult(int requestCode, int resultCode, Intent data) 
 	{
 		MercuryActivity.LogLocal("["+Channelname+"] onActivityResult(int requestCode, int resultCode, Intent data)");
+		if (callBackManager != null) {
+			callBackManager.onActivityResult(requestCode, resultCode, data); }
 	}
 	@Override
 	public void onNewIntent(Intent intent) 
