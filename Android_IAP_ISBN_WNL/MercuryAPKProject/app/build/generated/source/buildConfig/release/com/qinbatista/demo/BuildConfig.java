45a9f11978784127e38445da0dd094e812afc1a1
qin yu peng <qin.batista@outlook.com>
2020-09-25 18:56:26 +0800

    更新万年历sdk

---
+++ Android_IAP_ISBN_WNL/MercuryAPKProject/app/build/generated/source/buildConfig/release/com/qinbatista/demo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qinbatista.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.qinbatista.demo";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 298;
+  public static final String VERSION_NAME = "1.0";
+}
