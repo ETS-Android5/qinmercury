d2c3f58f92ef61e1b1f9e5f7e5514bdfa02e4b82
qin yu peng <qin.batista@outlook.com>
2020-10-15 18:00:41 +0800

    更新

--- z_PythonCode/Android_BASE__Template/MercuryJarProject/mercury/src/main/java/com/mercury/game/InAppDialog/IDCardVerifyDialog.java
+++ z_PythonCode/Android_BASE__Template/MercuryJarProject/mercury/src/main/java/com/mercury/game/InAppDialog/IDCardVerifyDialog.java
@@ -98,25 +98,25 @@ public class IDCardVerifyDialog {
         final ProgressBar progressBar = myLayout.findViewById(loadingId);
 //        final Button  cancelButton = myLayout.findViewById(cancelId);
 
-        cardIdEditText.setKeyListener(new NumberKeyListener() {
-            @NonNull
-            @Override
-            protected char[] getAcceptedChars() {
-                char[] c = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                        'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
-                        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-                return c;
-
-            }
-
-            @Override
-            public int getInputType() {
-                return 3;
-            }
-        });
+//        cardIdEditText.setKeyListener(new NumberKeyListener() {
+//            @NonNull
+//            @Override
+//            protected char[] getAcceptedChars() {
+//                char[] c = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+//                        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+//                        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+//                        'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+//                        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+//                        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+//                return c;
+//
+//            }
+//
+//            @Override
+//            public int getInputType() {
+//                return 3;
+//            }
+//        });
 
 
         progressBar.setVisibility(View.INVISIBLE);
--- z_PythonCode/Android_BASE__Template/MercuryJarProject/mercury/src/main/java/com/mercury/game/InAppDialog/LoginDialog.java
+++ z_PythonCode/Android_BASE__Template/MercuryJarProject/mercury/src/main/java/com/mercury/game/InAppDialog/LoginDialog.java
@@ -134,24 +134,24 @@ public class LoginDialog {
 //            }
 //        });
 
-        passwordEditText.setKeyListener(new NumberKeyListener() {
-            @NonNull
-            @Override
-            protected char[] getAcceptedChars() {
-                char[] c = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                        'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
-                        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-                        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-                return c;
-            }
-
-            @Override
-            public int getInputType() {
-                return 3;
-            }
-        });
+//        passwordEditText.setKeyListener(new NumberKeyListener() {
+//            @NonNull
+//            @Override
+//            protected char[] getAcceptedChars() {
+//                char[] c = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+//                        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+//                        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+//                        'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+//                        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+//                        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+//                return c;
+//            }
+//
+//            @Override
+//            public int getInputType() {
+//                return 3;
+//            }
+//        });
 
 
         progressBar.setVisibility(View.INVISIBLE);
