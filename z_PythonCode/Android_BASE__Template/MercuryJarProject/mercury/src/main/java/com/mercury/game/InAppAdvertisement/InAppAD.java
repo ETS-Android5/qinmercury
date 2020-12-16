c820ae86d64ab0eede4150e260af5ef434f30cb9
qin yu peng <qin.batista@outlook.com>
2020-12-16 15:26:43 +0800

    目前广告可控

--- z_PythonCode/Android_BASE__Template/MercuryJarProject/mercury/src/main/java/com/mercury/game/InAppAdvertisement/InAppAD.java
+++ z_PythonCode/Android_BASE__Template/MercuryJarProject/mercury/src/main/java/com/mercury/game/InAppAdvertisement/InAppAD.java
@@ -1,4 +1,5 @@
 package com.mercury.game.InAppAdvertisement;
+import com.mercury.game.InAppRemote.ADConfig;
 import com.mercury.game.MercuryApplication;
 import com.mercury.game.util.APPBaseInterface;
 import com.mercury.game.util.InAppBase;
@@ -10,12 +11,30 @@ import android.app.Application;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Looper;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+import static com.mercury.game.MercuryActivity.DeviceId;
+import static com.mercury.game.MercuryActivity.GameName;
+import static com.mercury.game.MercuryApplication.channelname;
 //end
+
 
 public class InAppAD extends InAppBase {
 	//comment
 	public static String appShow="InAppAD";
 	public static String MyScence = "";
+	public static boolean isAdAccessable = true;
 	public void ActivityInit(Activity context,final APPBaseInterface appcall)
 	{
 		super.ActivityInit(context, appcall);
@@ -70,93 +89,125 @@ public class InAppAD extends InAppBase {
 		super.attachBaseContext(base);
 	}
 	public void ActiveInterstitial() {
-		// TODO Auto-generated method stub
-		MercuryActivity.LogLocal("["+appShow+"] ActiveInterstitial");
-		AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-		builder.setMessage("Testing Mode");
-		builder.setTitle("ActiveInterstitial");
-		builder.setPositiveButton("Success", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				AdShowSuccessCallBack("ActiveInterstitial");
-				AdLoadSuccessCallBack("ActiveInterstitial");
-			}
-		});
-		builder.setNeutralButton("Failed", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				AdShowFailedCallBack("ActiveInterstitial");
-			}
-		});
-		builder.setCancelable(false);
-		builder.create().show();
+		ADConfig.isAdAccessable(
+				GameName, new Callback() {
+					@Override
+					public void onFailure(Call call, IOException e) {
+						onFailureAction("无法获取到该游戏的广告权限");
+					}
+					@Override
+					public void onResponse(Call call, Response response) throws IOException {
+						onActiveAction(response,"ActiveInterstitial");
+					}
+				});
 	}
 	public void ActiveBanner() {
-		// TODO Auto-generated method stub
-		MercuryActivity.LogLocal("["+appShow+"] ActiveBanner");
-		AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-		builder.setMessage("Testing Mode");
-		builder.setTitle("ActiveBanner");
-		builder.setPositiveButton("Success", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				AdShowSuccessCallBack("ActiveBanner");
-				AdLoadSuccessCallBack("ActiveBanner");
-			}
-		});
-		builder.setNeutralButton("Failed", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				AdShowFailedCallBack("ActiveBanner");
-			}
-		});
-		builder.setCancelable(false);
-		builder.create().show();
+		ADConfig.isAdAccessable(
+				GameName, new Callback() {
+					@Override
+					public void onFailure(Call call, IOException e) {
+						onFailureAction("无法获取到该游戏的广告权限");
+					}
+					@Override
+					public void onResponse(Call call, Response response) throws IOException {
+						onActiveAction(response,"ActiveBanner");
+					}
+				}
+				);
 	}
 	public void ActiveNative() {
-		// TODO Auto-generated method stub
-		MercuryActivity.LogLocal("["+appShow+"] ActiveNative");
-		AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-		builder.setMessage("Testing Mode");
-		builder.setTitle("ActiveNative");
-		builder.setPositiveButton("Success", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				AdShowSuccessCallBack("ActiveNative");
-				AdLoadSuccessCallBack("ActiveNative");
-			}
-		});
-		builder.setNeutralButton("Failed", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				AdShowFailedCallBack("ActiveNative");
-			}
-		});
-		builder.setCancelable(false);
-		builder.create().show();
+		ADConfig.isAdAccessable(
+				GameName, new Callback() {
+					@Override
+					public void onFailure(Call call, IOException e) {
+						onFailureAction("无法获取到该游戏的广告权限");
+					}
+					@Override
+					public void onResponse(Call call, Response response) throws IOException {
+						onActiveAction(response,"ActiveNative");
+					}
+				}
+		);
 	}
 	public void ActiveRewardVideo() {
-		// TODO Auto-generated method stub
-		MercuryActivity.LogLocal("["+appShow+"] ActiveRewardVideo");
+		ADConfig.isAdAccessable(
+				GameName, new Callback() {
+					@Override
+					public void onFailure(Call call, IOException e) {
+						onFailureAction("ActiveNative");
+					}
+					@Override
+					public void onResponse(Call call, Response response) throws IOException {
+						onActiveAction(response,"ActiveRewardVideo");
+					}
+				}
+		);
+	}
+	//end
+
+	public void onFailureAction(String msg) {
+		Looper.prepare();
+		Toast.makeText(mContext,msg,Toast.LENGTH_SHORT).show();
+		AdShowFailedCallBack("ActiveNative");
+		Looper.loop();
+	}
+
+	//接入sdk的时候根据传入的接口名作不同的处理
+	public void onActiveAction(Response response, String interfaceName) throws IOException{
+		String s = response.body().string();
+		Looper.prepare();
+		if (s != null) {
+			getAdAccessable(s,interfaceName);
+			alertADDialogShow(interfaceName,"Testing Mode",interfaceName);
+		}else {
+			Toast.makeText(mContext, "无法获取到该游戏的广告权限",Toast.LENGTH_SHORT).show();
+			AdShowFailedCallBack("ActiveNative");
+		}
+		Looper.loop();
+	}
+
+	public void alertADDialogShow(String title, String mode, final String interfaceName){
+		MercuryActivity.LogLocal("["+appShow+"]" + interfaceName);
 		AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-		builder.setMessage("Testing Mode");
-		builder.setTitle("ActiveRewardVideo");
+		builder.setMessage(mode);
+		builder.setTitle(title);
 		builder.setPositiveButton("Success", new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				AdShowSuccessCallBack("ActiveRewardVideo");
-				AdLoadSuccessCallBack("ActiveRewardVideo");
+				AdShowSuccessCallBack(interfaceName);
+				AdLoadSuccessCallBack(interfaceName);
 			}
 		});
 		builder.setNeutralButton("Failed", new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				AdShowFailedCallBack("ActiveRewardVideo");
+				AdShowFailedCallBack(interfaceName);
 			}
 		});
 		builder.setCancelable(false);
 		builder.create().show();
 	}
-	//end
+
+	public void getAdAccessable(String s,String interfaceName){
+		JSONObject json = null;
+		try {
+			json = (JSONObject) new JSONTokener(s).nextValue();
+			JSONObject data = json.getJSONObject("data");
+			if (data.isNull("isAdAccessable")) {
+				JSONObject result = data.getJSONObject("result");
+				isAdAccessable = result.isNull(channelname) ? true : result.getJSONObject(channelname).getBoolean("isAdAccessable");
+			} else {
+				isAdAccessable = data.getBoolean("isAdAccessable");
+			}
+			if (!isAdAccessable) {
+				Toast.makeText(mContext, "无法获取到该游戏的广告权限", Toast.LENGTH_SHORT).show();
+				AdShowFailedCallBack(interfaceName);
+				return;
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
 
---
+++ z_PythonCode/Android_BASE__Template/MercuryJarProject/mercury/src/main/java/com/mercury/game/InAppRemote/ADConfig.java
@@ -0,0 +1,30 @@
+package com.mercury.game.InAppRemote;
+
+import com.mercury.game.InAppDialog.LoginDialog;
+
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+import static com.mercury.game.MercuryActivity.LogLocal;
+
+public class ADConfig {
+    private static String adAccessableUrl = "https://gamesupportcluster.singmaan.com:10009/isadaccessable?game_name=%s";
+    public static void isAdAccessable(String game_name, Callback callback){
+        try {
+            String url = String.format(adAccessableUrl,game_name);
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            client.newCall(request).enqueue(callback);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            LogLocal("[ADConfig][isAdAccessable]");
+        }
+    }
+
+}
